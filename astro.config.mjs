300937f4d63a3a6c300d134c0ba03954c443f173
mychatterbox <mychatterbox@gmail.com>
2024-08-20 00:18:04 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://chatter.kr",
+  site: 'https://chatter.kr',
 
   trailingSlash: "ignore",
   integrations: [
