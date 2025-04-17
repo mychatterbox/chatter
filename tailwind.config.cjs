84d61e3f33e7fcf2ca643df29aef728dd959fa0d
mychatterbox <mychatterbox@gmail.com>
2025-04-18 01:50:45 +0900

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,29 +17,14 @@ module.exports = {
       typography: {
         DEFAULT: {
           css: {
-            'code::before': {
-              content: 'none', // 백틱 제거
-            },
-            'code::after': {
-              content: 'none', // 백틱 제거
-            },
-            blockquote: {
-              fontStyle: 'normal', // 이탤릭 효과 제거
-              quotes: 'none', // 따옴표 제거
-            },
             'code': {
               // color: '#d95d5d', // 인라인 코드 색상 (라이트 모드)
               fontFamily: 'inherit',
               fontSize: '1em',
             },
-            'pre code': {
-              color: 'inherit', // 코드 블록 색상 (기본값 유지)
-            },
-            '.dark code': {
-              // color: '#d95d5d', // 인라인 코드 색상 (다크 모드)
-            },
-            '.dark pre code': {
-              color: 'inherit', // 코드 블록 색상 (기본값 유지)
+            blockquote: {
+              fontStyle: 'normal', // 이탤릭 효과 제거
+              quotes: 'none', // 따옴표 제거
             },
             // hr 스타일 추가
             hr: {
