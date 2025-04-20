2397f0fe35a92d650f4d00a265c65a2a59a020ab
mychatterbox <mychatterbox@gmail.com>
2025-04-20 15:06:57 +0900

    ogimage 원래대로

--- src/content.config.ts
+++ src/content.config.ts
@@ -10,9 +10,7 @@ const blog = defineCollection({
     pubDate: z.date(),
     kind: z.enum(['article', 'note']),
     list: z.boolean().optional(),
-    ogImage: z.string().refine(val => val.startsWith('/assets/'), {
-      message: "OG 이미지 경로는 '/assets/'로 시작해야 합니다"
-    }),
+    ogImage: z.string().optional(),
   })
   .transform((o) => {
     return { ...o, draft: !(o.title && o.description && o.pubDate) };
