f79778cdbdc4ba78c0eaff00645740da4a2f1df0
mychatterbox <mychatterbox@gmail.com>
2025-05-04 01:52:12 +0900

    umami 적용

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import FlexokiDark from "/src/styles/Flexoki-Dark-color-theme.json";
 import FlexokiLight from "/src/styles/Flexoki-Light-color-theme.json";
+import umami from "@yeskunall/astro-umami";
 
 export default defineConfig({
   site: "https://chatter.kr/",
@@ -16,6 +17,7 @@ export default defineConfig({
     sitemap({
       filter: (p) => !p.includes("/draft/"),
     }),
+    umami({ id: "4811eb93-de23-464b-a636-82f4ab7af5b9" }),
   ],
 
   markdown: {
