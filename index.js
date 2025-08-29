9ba7c2d6e6126051f95682939b53af215c4a32cb
mychatterbox <mychatterbox@gmail.com>
2025-08-29 16:06:05 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+// index.js
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
-
-    // 루트('/')는 제외하고, 끝이 '/'로 끝나는 경우만 처리
+    
+    // 루트 경로가 아니면서 슬래시로 끝나는 경우
     if (url.pathname !== "/" && url.pathname.endsWith("/")) {
-      // 마지막 '/' 제거
-      const newPath = url.pathname.replace(/\/$/, "");
-      const newUrl = `${url.origin}${newPath}${url.search}`;
+      const newPath = url.pathname.replace(/\/+$/, ""); // 여러 개의 슬래시도 제거
+      const newUrl = `${url.origin}${newPath}${url.search}${url.hash}`;
+      
+      // 301 영구 이동
       return Response.redirect(newUrl, 301);
     }
-
-    // 나머지 요청은 정적 파일 처리
+    
+    // 정적 파일 서빙
     return env.ASSETS.fetch(request);
   }
 }
