bc59ce4ea42725cf0def1d3ef9a4ed4d5d63fa15
mychatterbox <mychatterbox@gmail.com>
2025-10-11 04:39:18 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import FlexokiDark from "/src/styles/Flexoki-Dark-color-theme.json";
 import FlexokiLight from "/src/styles/Flexoki-Light-color-theme.json";
-// import umami from "@yeskunall/astro-umami";
+import umami from "@yeskunall/astro-umami";
 
 export default defineConfig({
   site: 'https://chatter.kr/',
@@ -23,7 +23,7 @@ export default defineConfig({
     sitemap({
       filter: (p) => !p.includes("/draft/"),
     }),
-    // umami({ id: "4811eb93-de23-464b-a636-82f4ab7af5b9" }),
+    umami({ id: "4811eb93-de23-464b-a636-82f4ab7af5b9" }),
   ],
 
   markdown: {
