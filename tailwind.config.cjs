7ebbbf549ba69dcd9cca6a149bda33c98498f3b7
mychatterbox <mychatterbox@gmail.com>
2025-10-10 17:28:58 +0900

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: [
-          ['"Pretendard"', ...defaultTheme.fontFamily.sans],
+          ['"Pretendard Variable"', ...defaultTheme.fontFamily.sans],
           { fontFeatureSettings: '"cv12"' },
         ],
         mono: [
