65fba187d0eff486e8fbdf301a8c3069548a6fa5
mychatterbox <mychatterbox@gmail.com>
2025-10-30 15:54:12 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
     layout: 'constrained',
   },
 
-  trailingSlash: "never",
+  trailingSlash: "ignore",
 
   build: {
     format: "preserve",
@@ -20,19 +20,6 @@ export default defineConfig({
   integrations: [
     sitemap({
       filter: (p) => !p.includes("/draft/"),
-      serialize: (item) => {
-        const url = new URL(item.url);
-
-        // 끝 슬래시 제거 후, 루트만 슬래시 유지
-        const pathname = url.pathname.replace(/\/$/, "");
-        let formattedUrl = `${url.origin}${pathname}`;
-        if (pathname === "") formattedUrl = `${url.origin}/`;
-
-        return {
-          ...item,
-          url: formattedUrl, // 문자열 그대로 반환 (URL 객체로 변환 금지)
-        };
-      },
     }),
     umami({ id: "4811eb93-de23-464b-a636-82f4ab7af5b9" }),
   ],
