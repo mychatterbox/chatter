5560e18034c940367c451b762ae684f0d5116ce7
mychatterbox <mychatterbox@gmail.com>
2025-11-05 02:40:24 +0900

    Delete prettier.config.js

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  printWidth: 100,
-  proseWrap: "always",
-  plugins: ["prettier-plugin-astro"],
-  overrides: [
-    { files: "*.astro", options: { parser: "astro" } },
-    { files: "*.md", options: { parser: "markdown" } },
-  ],
-};
