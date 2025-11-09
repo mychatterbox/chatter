96baae02e06c07c1b58dfb2241b0ff72891213ee
mychatterbox <mychatterbox@gmail.com>
2025-11-10 00:55:15 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import FlexokiDark from "./src/styles/Flexoki-Dark-color-theme.json";
 import FlexokiLight from "./src/styles/Flexoki-Light-color-theme.json";
 import umami from "@yeskunall/astro-umami";
 
+import playformInline from "@playform/inline";
+
 export default defineConfig({
   site: 'https://chatter.kr/',
 
@@ -28,7 +30,7 @@ export default defineConfig({
     }
     return item;
   },
-  }), umami({ id: "4811eb93-de23-464b-a636-82f4ab7af5b9" }),],
+  }), umami({ id: "4811eb93-de23-464b-a636-82f4ab7af5b9" }), playformInline()],
 
   markdown: {
     shikiConfig: {
