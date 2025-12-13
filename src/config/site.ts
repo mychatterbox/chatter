f11a5bb718d1724ad29bf81b4f39dea879987afd
mychatterbox <mychatterbox@gmail.com>
2025-12-14 01:38:30 +0900

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,7 +3,7 @@
  * Single source of truth for site metadata
  */
 
-export const SITE_URL = 'https://who.chatterer.workers.dev';
+export const SITE_URL = 'https://chatter.kr';
 export const SITE_URL_WITH_SLASH = `${SITE_URL}/`;
-export const SITE_NAME = 'who.chatterer.workers.dev';
+export const SITE_NAME = 'mychatterbox';
 export const SITE_OG_IMAGE = `${SITE_URL}/images/no_ogimage.png`;
